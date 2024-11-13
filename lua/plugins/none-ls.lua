f7ce2fc6104686165f979ad25652107e787912f7
Bennet Bechtel <bennet.bechtel007@gmail.com>
2024-11-13 22:33:43 +0100

    none-ls formatting fix

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,4 +1,3 @@
--- Format on save and linters
 return {
 	"nvimtools/none-ls.nvim",
 	dependencies = {
@@ -7,26 +6,26 @@ return {
 	},
 	config = function()
 		local null_ls = require("null-ls")
-		local formatting = null_ls.builtins.formatting -- to setup formatters
-		local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+		local formatting = null_ls.builtins.formatting
+		local diagnostics = null_ls.builtins.diagnostics
 
-		-- list of formatters & linters for mason to install
+		-- Mason setup for formatters and linters
 		require("mason-null-ls").setup({
 			ensure_installed = {
 				"prettier", -- ts/js formatter
 				"stylua", -- lua formatter
 				"eslint_d", -- ts/js linter
-				"shfmt",
-				"ruff",
+				"shfmt", -- shell formatter
+				"ruff", -- python formatter/linter
 			},
-			-- auto-install configured formatters & linters (with null-ls)
 			automatic_installation = true,
 		})
 
+		-- Configure formatting sources
 		local sources = {
-			-- Prettier setup with Tailwind CSS plugin support
+			-- Prettier with Tailwind CSS plugin support
 			formatting.prettier.with({
-				extra_args = { "--plugin", "prettier-plugin-tailwindcss" }, -- Load Tailwind CSS plugin
+				extra_args = { "--plugin", "prettier-plugin-tailwindcss" },
 				filetypes = {
 					"html",
 					"json",
@@ -45,12 +44,15 @@ return {
 			require("none-ls.formatting.ruff_format"),
 		}
 
+		-- Create format-on-save autocommand group
 		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+		-- Setup none-ls
 		null_ls.setup({
-			-- debug = true, -- Enable debug mode. Inspect logs with :NullLsLog.
+			-- debug = true,  -- Enable for troubleshooting
 			sources = sources,
-			-- you can reuse a shared lspconfig on_attach callback here
 			on_attach = function(client, bufnr)
+				-- Enable format on save for clients that support formatting
 				if client.supports_method("textDocument/formatting") then
 					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 					vim.api.nvim_create_autocmd("BufWritePre", {
@@ -64,12 +66,14 @@ return {
 			end,
 		})
 
-		-- format on save
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			pattern = "*", -- Apply to all file types
-			callback = function()
-				vim.lsp.buf.format({ async = false }) -- Format synchronously before saving
-			end,
-		})
+		-- Note: The following global format-on-save autocmd might be redundant
+		-- since formatting is already handled in the on_attach function above
+		-- Consider removing this if you don't need it
+		-- vim.api.nvim_create_autocmd("BufWritePre", {
+		--     pattern = "*",
+		--     callback = function()
+		--         vim.lsp.buf.format({ async = false })
+		--     end,
+		-- })
 	end,
 }
